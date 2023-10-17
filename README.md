03061808761224f140ae68179c1adc10b30bbc3a
twk <1024565378@qq.com>
2023-10-17 21:42:35 +0800

    !

--- README.md
+++ README.md
@@ -1,28 +1,35 @@
 ## 快速建立图片特征数据库并用文本描述进行搜索
 >基于CLIP官方项目整理：https://github.com/openai/CLIP  
->采用国内配套中文模型：https://huggingface.co/IDEA-CCNL/Taiyi-CLIP-Roberta-large-326M-Chinese  
->首次运行代码时会自动下载模型文件到本地clip(890M)和huggingface(1.4G)文件夹中(共约2.3G)  
+>国内配套中文文本模型：https://huggingface.co/IDEA-CCNL/Taiyi-CLIP-Roberta-large-326M-Chinese  
+>首次运行代码时会自动下载clip模型文件到本地clip(890M)和huggingface(1.4G)文件夹中(共约2.3G)  
 >GPU运行时显存占用约2500M  
 ### CLIP介绍
 >CLIP是2021年openai发布的基于对比学习的多模态模型(pytorch)，用于将文字描述和图片匹配  
 >由一个图片编码模型和一个文本编码模型组成，一张图片经过图片模型得到的特征向量和这张图片的描述经过文本模型得到的特征向量会相近  
->通过计算余弦相似度和设定阈值可以通过文本找图片、通过图片找文本  
+>通过计算余弦相似度可以通过文本找图片、通过图片找文本  
 >原CLIP官方文本模型只支持英文，国内有人训练了中文的文本模型，但只支持ViT-L/14型号(890M)  
 ### 项目介绍
 >本项目将建立图片特征数据库，然后用文本描述(75字以内，长的会被截断)去搜索符合描述的图片  
-### 1，database_prepare.py
+### 1.环境：linux
+>torch==1.12.0：https://pytorch.org/get-started/previous-versions/
+>```
+>pip install ftfy regex tqdm transformers -i https://pypi.tuna.tsinghua.edu.cn/simple
+>pip install git+https://github.com/openai/CLIP.git
+>```
+### 2.下载中文文本模型(13G)
+>```
+>sudo apt-get install git-lfs：安装git-lfs
+>git lfs install：启用lfs。不使用lfs无法下载大文件
+>git clone https://huggingface.co/IDEA-CCNL/Taiyi-CLIP-Roberta-large-326M-Chinese：下载中文文本模型
+>```
+### 3，database_prepare.py
 >将数据库图片放入文件夹image_database中  
 >运行database_prepare.py即可生成特征数据库feature_database.csv  
-### 2，predict.py
->在english_text、chinese_text中输入英文、中文文本，运行程序后可以搜到数据库中符合文本描述的图片  
->args中english_score_threshold、chinese_score_threshold为匹配的相似度筛选阈值，0.17、0.12为基准，可适当调整  
-### 3，flask_start.py
+### 4，predict.py
+>在text中输入文本，运行程序后可以搜到数据库中符合文本描述的图片  
+### 5，flask_start.py
 >用flask将程序包装成一个服务，并在服务器上启动
-### 4，flask_request.py
+### 6，flask_request.py
 >以post请求传输数据调用服务
-### 5，gunicorn_config.py
+### 7，gunicorn_config.py
 >用gunicorn多进程启动flask服务：gunicorn -c gunicorn_config.py flask_start:app
-### 其他
->github链接：https://github.com/TWK2022/clip  
->学习笔记：https://github.com/TWK2022/notebook  
->邮箱：1024565378@qq.com  
--- predict.py
+++ predict.py
@@ -8,76 +8,69 @@ import pandas as pd
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--database_path', default='feature_database.csv', type=str, help='|特征数据库位置|')
 parser.add_argument('--model_name', default='ViT-L/14', type=str, help='|模型名称，中文文本模型只支持ViT-L/14(890M)|')
-parser.add_argument('--english_score_threshold', default=0.17, type=int, help='|英文文本相似度筛选阈值，0.17为基准|')
-parser.add_argument('--chinese_score_threshold', default=0.12, type=int, help='|中文文本相似度筛选阈值，0.12为基准|')
+parser.add_argument('--chinese_cache', default='/root/.cache/huggingface/hub', type=str, help='|中文文本模型缓存|')
+parser.add_argument('--use_chinese', default=False, type=bool, help='|True时输入文本为中文，False时为英文|')
 parser.add_argument('--device', default='cuda', type=str, help='|运行设备|')
 args = parser.parse_args()
 
 
-class predict:
+class clip_class:
     def __init__(self, args):
         self.device = args.device
         self.model_name = args.model_name
         self.database_path = args.database_path
-        self.english_score_threshold = args.english_score_threshold
-        self.chinese_score_threshold = args.chinese_score_threshold
+        self.use_chinese = args.use_chinese
         # 模型
         model, image_deal = clip.load(self.model_name, device=self.device)  # clip模型：图片模型+英文文本模型
-        chinese_encode = transformers.BertForSequenceClassification.from_pretrained(
-            "IDEA-CCNL/Taiyi-CLIP-Roberta-large-326M-Chinese",
-            cache_dir='/root/.cache/huggingface/hub').eval().half().to(self.device)  # 中文文本模型，只支持ViT-L/14(890M)
-        print(f'| 模型加载成功:{self.model_name} | 中文文本模型:IDEA-CCNL/Taiyi-CLIP-Roberta-large-326M-Chinese |')
         self.model = model.eval()
-        self.chinese_encode = chinese_encode
+        if args.use_chinese:
+            chinese_tokenizer = transformers.BertTokenizer.from_pretrained(
+                "IDEA-CCNL/Taiyi-CLIP-Roberta-large-326M-Chinese", cache_dir='/root/.cache/huggingface/hub')
+            self.chinese_tokenizer = chinese_tokenizer
+            chinese_encode = transformers.BertForSequenceClassification.from_pretrained(
+                "IDEA-CCNL/Taiyi-CLIP-Roberta-large-326M-Chinese",
+                cache_dir=args.chinese_cache).eval().half().to(self.device)  # 中文文本模型，只支持ViT-L/14(890M)
+            self.chinese_encode = chinese_encode
         # 数据
         df = pd.read_csv(self.database_path)
         column = df.columns
         image_feature = df.values
         self.column = column
         self.image_feature = image_feature
+        print(f'| 初始化完成 |')
 
-    def _predict(self, english_text=None, chinese_text=None):  # 输入单个/多个文本，返回图片名和概率值
+    def _deal(self, text_feature):  # 输入单个/多个文本，返回大于阈值的图片名和相似度
+        text_feature /= torch.norm(text_feature, dim=1, keepdim=True)  # 归一化
+        text_feature = text_feature.cpu().numpy()
+        score = np.dot(text_feature, self.image_feature)
+        index_list = [np.argmax(_) for _ in score]
+        column = [self.column[_] for _ in index_list]
+        score = [i[j] for i, j in zip(score, index_list)]
+        return column, score
+
+    def predict(self, text):  # 输入单个/多个文本，返回图片名和概率值
         with torch.no_grad():
             # 英文
-            english_colunm = None
-            if english_text:
-                english_sequence = clip.tokenize(english_text).to(self.device)  # 处理
+            if not self.use_chinese:
+                english_sequence = clip.tokenize(text).to(self.device)  # 处理
                 english_text_feature = self.model.encode_text(english_sequence)  # 推理
-                english_text_feature /= torch.norm(english_text_feature, dim=1, keepdim=True)  # 归一化
-                english_text_feature = english_text_feature.cpu().numpy()
-                english_colunm, english_score = self._deal(english_text_feature, self.english_score_threshold)
+                column, score = self._deal(english_text_feature)
             # 中文
-            chinese_colunm = None
-            if chinese_text:
-                chinese_tokenizer = transformers.BertTokenizer.from_pretrained(
-                    "IDEA-CCNL/Taiyi-CLIP-Roberta-large-326M-Chinese", cache_dir='/root/.cache/huggingface/hub')
-                chinese_sequence = chinese_tokenizer(chinese_text, max_length=77, padding='max_length',
-                                                     truncation=True, return_tensors='pt')['input_ids'].type(
+            else:
+                chinese_sequence = self.chinese_tokenizer(text, max_length=77, padding='max_length',
+                                                          truncation=True, return_tensors='pt')['input_ids'].type(
                     torch.int32).to(self.device)  # 处理
                 chinese_text_feature = self.chinese_encode(chinese_sequence).logits  # 推理
-                chinese_text_feature /= torch.norm(chinese_text_feature, dim=1, keepdim=True)  # 归一化
-                chinese_text_feature = chinese_text_feature.cpu().numpy()
-                chinese_colunm, chinese_score = self._deal(chinese_text_feature, self.chinese_score_threshold)
-        return english_colunm, english_score, chinese_colunm, chinese_score
-
-    def _deal(self, text_feature, score_threshold):  # 输入单个/多个文本，返回大于阈值的图片名和相似度
-        score = np.dot(text_feature, self.image_feature)
-        judge = np.where(score > score_threshold, True, False)
-        colunm = [self.column[judge[_]].tolist() for _ in range(len(judge))]
-        score = [score[_][judge[_]].tolist() for _ in range(len(judge))]
-        return colunm, score
+                column, score = self._deal(chinese_text_feature)
+        return column, score
 
 
 if __name__ == '__main__':
     # 输入文本
-    english_text = ['Lipstick', 'Cat', 'Office']
-    chinese_text = ['口红', '猫', '办公室']
+    text = ['Lipstick', 'Cat', 'Office']
     # 开始预测
-    predictor = predict(args)
-    english_colunm, english_score, chinese_colunm, chinese_score = \
-        predictor._predict(english_text, chinese_text)  # 输入单个/多个文本，返回大于阈值的图片名和相似度
-    print(f'| 英文阈值:{args.english_score_threshold}  中文阈值:{args.chinese_score_threshold} |')
-    print(f'| 英文{english_text}:{english_colunm} |')
-    print(f'| 相似度:{english_score} |')
-    print(f'| 中文{chinese_text}:{chinese_colunm} |')
-    print(f'| 相似度:{chinese_score} |')
+    model = clip_class(args)
+    column, score = model.predict(text)  # 输入单个/多个文本，最匹配的图片和相似度
+    print(f'| 输入:{text} |')
+    print(f'| 图片:{column} |')
+    print(f'| 相似度:{score} |')
