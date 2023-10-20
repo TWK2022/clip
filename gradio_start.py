044f6f1c26eebbe9338d99b61d3dfc6bad7bc08a
TWK <109967047+TWK2022@users.noreply.github.com>
2023-10-20 13:42:06 +0800

    !

--- gradio_start.py
+++ gradio_start.py
@@ -12,7 +12,8 @@ from predict import clip_class
 parser = argparse.ArgumentParser('|在服务器上启动gradio服务|')
 parser.add_argument('--database_path', default='feature_database.csv', type=str, help='|特征数据库位置|')
 parser.add_argument('--model_name', default='ViT-L/14', type=str, help='|模型名称，中文文本模型只支持ViT-L/14(890M)|')
-parser.add_argument('--chinese_model', default='clip_chinese_model', type=str, help='|中文文本模型名称或下载位置|')
+parser.add_argument('--chinese_model', default='clip_chinese_model', type=str,
+                    help='|IDEA-CCNL/Taiyi-CLIP-Roberta-large-326M-Chinese或模型下载位置|')
 parser.add_argument('--device', default='cpu', type=str, help='|运行设备|')
 args = parser.parse_args()
 
