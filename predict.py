4f450c9348ed98da1d083cfea3c0252339d11d56
TWK <109967047+TWK2022@users.noreply.github.com>
2023-10-20 16:46:17 +0800

    !

--- predict.py
+++ predict.py
@@ -9,7 +9,8 @@ import pandas as pd
 os.environ["KMP_DUPLICATE_LIB_OK"] = 'TRUE'  # 防止可能出现的libiomp5.dylib报错
 parser = argparse.ArgumentParser(description='|clip文本搜图片|')
 parser.add_argument('--database_path', default='feature_database.csv', type=str, help='|特征数据库位置|')
-parser.add_argument('--model_name', default='ViT-L/14', type=str, help='|模型名称，中文文本模型只支持ViT-L/14(890M)|')
+parser.add_argument('--model_name', default='ViT-L/14', type=str,
+                    help='|模型名称或模型下载位置，中文文本模型只支持ViT-L/14(890M)|')
 parser.add_argument('--chinese_model', default='clip_chinese_model', type=str,
                     help='|IDEA-CCNL/Taiyi-CLIP-Roberta-large-326M-Chinese或模型下载位置|')
 parser.add_argument('--device', default='cpu', type=str, help='|运行设备|')
