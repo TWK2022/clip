a00a430fa4c6edbb5be5facf6a711e461ea94488
twk <1024565378@qq.com>
2023-10-27 21:40:30 +0800

    !

--- block/data_get.py
+++ block/data_get.py
@@ -9,7 +9,7 @@ def data_get(args):
 
 class data_prepare(object):
     def __init__(self, args):
-
+        self.data_path = args.data_path
 
     def _load(self):
         # 读取数据
@@ -17,4 +17,7 @@ class data_prepare(object):
             df = pd.read_csv(self.data_path, encoding='utf-8')
         except:
             df = pd.read_csv(self.data_path, encoding='gbk')
+        input_data = df.columns
+        output_data = df.values.T.astype(np.float32)
+        data_dict = {'input_data': input_data, 'output_data': output_data}
         return data_dict
