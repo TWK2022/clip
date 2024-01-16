3818fe0a3559ea77f63f422f43994beee32431e3
TWK <109967047+TWK2022@users.noreply.github.com>
2024-01-16 15:48:05 +0800

    !

--- block/model_get.py
+++ block/model_get.py
@@ -4,5 +4,8 @@ import transformers
 def model_get(args):
     model = transformers.BertForSequenceClassification.from_pretrained(args.model)
     model_dict = {'model': model}
+    model_dict['epoch'] = 0  # 已训练的轮次
+    model_dict['optimizer_state_dict'] = None  # 学习率参数
+    model_dict['lr_adjust_index'] = 0  # 学习率调整次数
     model_dict['ema_updates'] = 0  # ema参数
     return model_dict
